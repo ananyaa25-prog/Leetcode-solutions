39bf8ea3a375961462d95a0c598bcd17a2036566
ananya07 <ambasthaananya@gmail.com>
2026-08-03 15:48:55 +0530

    Add solution for Merge Intervals

--- LeetCode/Merge-Intervals/Solution.cpp
+++ LeetCode/Merge-Intervals/Solution.cpp
@@ -1,25 +1,2 @@
-1class Solution {
-2public:
-3    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-4           sort(intervals.begin(),intervals.end());
-5           vector<vector<int>> ans;
-6            int i=0;
-7           while(i<intervals.size())
-8           {
-9            vector<int> row;
-10            int start=intervals[i][0];
-11            int end=intervals[i][1];
-12            while(i+1<intervals.size() && intervals[i+1][0]<=end)
-13            {
-14                end=max(end,intervals[i+1][1]);
-15                i++;
-16            }
-17            row.push_back(start);
-18            row.push_back(end);
-19            
-20            ans.push_back(row);
-21            i++;
-22           }  
-23           return ans;                 
-24    }
-25};
+left_res = [ (intervals that do not overlap), (intervals that overlap with current node) ]
+right_res = [ (intervals that overlap with current node), (intervals that do not overlap) ]
