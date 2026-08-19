9c3e254a73ad88f6f82d00cf50406b661bc2b0de
ananya07 <ambasthaananya@gmail.com>
2026-08-19 14:38:52 +0530

    Add solution for Binary Search

--- LeetCode/Binary-Search/Solution.cpp
+++ LeetCode/Binary-Search/Solution.cpp
@@ -1,23 +1,25 @@
 1class Solution {
 2public:
 3    int search(vector<int>& nums, int target) {
-4        int l = 0;
-5        int r = nums.size() - 1;
-6
-7        while(l <= r) {
-8            int mid = l + (r - l) / 2;
-9
-10            if(nums[mid] == target) {
+4        int l=0;
+5        int r=nums.size()-1;
+6        while(l<=r)
+7        {
+8            int mid= l + (r - l) / 2;
+9            if(nums[mid]==target)
+10            {
 11                return mid;
 12            }
-13            else if(nums[mid] < target) {
-14                l = mid + 1;     // target is on RIGHT
-15            }
-16            else {
-17                r = mid - 1;     // target is on LEFT
-18            }
-19        }
-20
-21        return -1;
-22    }
-23};
+13            else if(nums[mid]<target)
+14            {
+15                l=mid+1;
+16            }
+17            else
+18            {
+19                r=mid-1;
+20            }
+21        }
+22        return -1;
+23
+24    }
+25};
