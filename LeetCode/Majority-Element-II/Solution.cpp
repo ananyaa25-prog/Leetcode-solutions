058a8676e3b8209764618137bfd6b6fd34edec05
ananya07 <ambasthaananya@gmail.com>
2026-08-20 22:29:42 +0530

    Add solution for Majority Element II

--- LeetCode/Majority-Element-II/Solution.cpp
+++ LeetCode/Majority-Element-II/Solution.cpp
@@ -1,22 +1,22 @@
 1class Solution {
 2public:
 3    vector<int> majorityElement(vector<int>& nums) {
-4       vector<int> ans;
-5       sort(nums.begin(),nums.end());
-6       int n=nums.size();
-7       for(int i=0;i<n;i++)
-8       {
-9        int count=1;
-10        while(i+1<n&& nums[i]==nums[i+1])
-11        {
-12            count++;
-13            i++;
-14        }
-15        if(count>n/3){
-16       ans.push_back(nums[i]);
-17       } 
-18       }
-19       return ans;
-20    }
-21};
-22
+4        sort(nums.begin(),nums.end());
+5        vector<int> ans;
+6        int n=nums.size();
+7        for(int i=0;i<n;i++)
+8        {
+9            int count=1;
+10            while( i+1<n && nums[i]==nums[i+1])
+11            {
+12               count++;
+13               i++;
+14            }
+15            if(count>n/3)
+16            {
+17                ans.push_back(nums[i]);
+18            }
+19        } 
+20        return ans;  
+21    }
+22};
