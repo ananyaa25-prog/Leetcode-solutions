8c7fc14e58864a1a7f03634fc3980a8f9f5aa128
ananya07 <ambasthaananya@gmail.com>
2026-08-22 15:26:47 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -1,12 +1,17 @@
- for(var i =0;i<nums.length;i++){
-    var item = nums[i];   
-    for(var j=0; j < nums.length;j++){
-        var item2 = nums[j];
-        var sum = item + item2;
-        
-    if(sum === target ){
-      return [i,j];
-      
-    }
-  }
- }
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        unordered_map<int,int> mpp;
+5        int n=nums.size();
+6        for(int i=0;i<n;i++)
+7        {
+8            int need=target-nums[i];
+9            if(mpp.find(need)!=mpp.end())
+10            {
+11                return{mpp[need],i};
+12            }
+13            mpp[nums[i]]=i;
+14        }
+15        return {};
+16    }
+17};
