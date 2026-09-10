dd7cf418a0048d62559765cfd52e5ba7b82868e6
ananya07 <ambasthaananya@gmail.com>
2026-09-10 17:21:06 +0530

    Add solution for Contains Duplicate

--- LeetCode/Contains-Duplicate/Solution.cpp
+++ LeetCode/Contains-Duplicate/Solution.cpp
@@ -1,16 +1,15 @@
 1class Solution {
 2public:
 3    bool containsDuplicate(vector<int>& nums) {
-4        unordered_set<int> st;
-5        for(int x:nums)
-6        {
-7            if(st.find(x)!=st.end())
-8            {
-9                return true;
-10            }
-11            st.insert(x);
-12        }
-13        return false;
+4      unordered_set<int> st;
+5      for(int x:nums)
+6      {
+7        if(st.find(x)!=st.end())
+8        {
+9            return true;
+10        }
+11        st.insert(x);
+12      }  
+13      return false;
 14    }
 15};
-16
