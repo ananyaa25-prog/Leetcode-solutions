fb87c71537a4347a3bcea0d9cdeb4eb292a51ad7
ananya07 <ambasthaananya@gmail.com>
2026-09-13 15:04:32 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.cpp
+++ LeetCode/Valid-Anagram/Solution.cpp
@@ -2,19 +2,19 @@
 2public:
 3    bool isAnagram(string s, string t) {
 4      unordered_map<char,int> mp1;
-5      unordered_map<char,int>mp2;
-6      for(char x:s)
-7      {
-8        mp1[x]++;
-9      }  
-10      for(char y:t)
-11      {
-12        mp2[y]++;
-13      }
-14      if(mp1==mp2)
-15      {
-16        return true;
-17      }
-18      return false;
+5      unordered_map<char,int> mp2;
+6        for(char c1:s)
+7        {
+8            mp1[c1]++;
+9        }
+10        for(char c2:t )
+11        {
+12            mp2[c2]++;
+13        }
+14        if(mp1==mp2)
+15        {
+16            return true;
+17        }
+18        return false;
 19    }
 20};
