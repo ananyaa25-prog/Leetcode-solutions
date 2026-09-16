9fcfad563ccb06754b2d3c812f4d34cd765dd868
ananya07 <ambasthaananya@gmail.com>
2026-09-16 19:10:35 +0530

    Add solution for Min Stack

--- LeetCode/Min-Stack/Solution.cpp
+++ LeetCode/Min-Stack/Solution.cpp
@@ -1,60 +1,64 @@
 1class MinStack {
-2        stack<long long>st;
-3        long long  min=LLONG_MAX;
+2    stack<long long >st;
+3    long long minV=LLONG_MAX;
 4public:
 5    MinStack() {
 6        
 7    }
-8    void push(int value) {
-9        if(st.empty())
-10        {
-11             min=value;
-12             st.push(value);
-13        }
-14        else{
-15            if(value>min) st.push(value);
-16            else
-17            {
-18                st.push(2LL*value-min);
-19                min=value;
+8    
+9    void push(int value) {
+10        if(st.empty())
+11        {
+12            minV=value;
+13            st.push(value);
+14        }
+15        else
+16        {
+17            if(value>minV)
+18            {
+19                st.push(value);
 20            }
-21        }
-22
-23    }
-24
-25    void pop() {
-26     if(st.empty())
-27     {
-28        return;
-29     }   
-30     long long x=st.top();
-31     st.pop();
-32     if(x<min)
-33     {
-34        min=2*min-x;
-35     }
-36    }
-37    
-38    int top() {
-39       if(st.empty())
-40       {
-41        return -1;
-42       } 
-43       long long  x=st.top();
-44       if(min<x) return x;
-45       return min;
-46    }
-47    
-48    int getMin() {
-49        return min;
+21            else
+22            {
+23                st.push(2LL*value-minV);
+24                minV=value;
+25            }
+26        }
+27    }
+28    
+29    void pop() {
+30        if(st.empty())
+31        {
+32            return;
+33        }
+34        long long x=st.top();
+35        st.pop();
+36        if(x<minV)
+37        {
+38            minV=2LL*minV-x;
+39        }
+40    }
+41    
+42    int top() {
+43       if(st.empty())
+44       {
+45        return -1;
+46       } 
+47       long long x=st.top();
+48       if(minV<x)  return x;
+49       return minV;
 50    }
-51};
-52
-53/**
-54 * Your MinStack object will be instantiated and called as such:
-55 * MinStack* obj = new MinStack();
-56 * obj->push(value);
-57 * obj->pop();
-58 * int param_3 = obj->top();
-59 * int param_4 = obj->getMin();
-60 */
+51    
+52    int getMin() {
+53        return minV;
+54    }
+55};
+56
+57/**
+58 * Your MinStack object will be instantiated and called as such:
+59 * MinStack* obj = new MinStack();
+60 * obj->push(value);
+61 * obj->pop();
+62 * int param_3 = obj->top();
+63 * int param_4 = obj->getMin();
+64 */
