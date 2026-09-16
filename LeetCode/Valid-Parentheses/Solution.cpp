d20e0b6ab8ebe85820e445fea0073f146ec3af52
ananya07 <ambasthaananya@gmail.com>
2026-09-16 18:03:10 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.cpp
+++ LeetCode/Valid-Parentheses/Solution.cpp
@@ -1,32 +1,32 @@
 1class Solution {
 2public:
 3    bool isValid(string s) {
-4        stack<char>st;
-5        int n=s.size();
-6        for(int i=0;i<n;i++)
-7        {
-8            if(s[i]=='('||s[i]=='{'||s[i]=='[')//opening
-9            {
-10                st.push(s[i]);
-11            }
-12            else
-13            {
-14               if(st.size()==0)
-15               {
+4       stack<char>st;
+5       int n=s.size();
+6       for(int i=0;i<n;i++)
+7       {
+8        if(s[i]=='('||s[i]=='{'||s[i]=='[')
+9        {
+10            st.push(s[i]);
+11        }
+12        else
+13        {
+14            if(st.size()==0)
+15            {
 16                return false;
-17               }
-18               if(st.top()=='('&& s[i]==')'||
-19                  st.top()=='{'&& s[i]=='}'||
-20                  st.top()=='['&& s[i]==']')
-21                  {
-22                    st.pop();
-23                  }
-24                else
-25                {
-26                    return false;
-27                }
-28            }
-29        }
-30        return st.size()==0;
+17            }
+18            if(st.top()=='{' && s[i]=='}'||
+19               st.top()=='(' && s[i]==')'||
+20               st.top()=='[' && s[i]==']')
+21               {
+22                st.pop();
+23               }
+24            else
+25            {
+26                return false;
+27            }
+28        }
+29       }
+30       return st.size()==0; 
 31    }
 32};
