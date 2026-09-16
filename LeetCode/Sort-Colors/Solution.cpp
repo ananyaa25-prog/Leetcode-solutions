6dd50368402a83e3812b6c6ba216d160f784200a
ananya07 <ambasthaananya@gmail.com>
2026-09-16 18:19:20 +0530

    Add solution for Sort Colors

--- LeetCode/Sort-Colors/Solution.cpp
+++ LeetCode/Sort-Colors/Solution.cpp
@@ -1,8 +1,45 @@
-[0]
-[2,0]
-[0,0,0,1,1,1,2,2,2]
-[2,2,2,0,0,0,1,1,1]
-[2,2,1,1,0,0,0]
-[2,1,2,2,2,2,0,0,0]
-[2,2,2,2,2,1,0,0,1,1,0]
-[2,2,2,1,1,0,0,1,0,1,0,2,1,0,2,1,0,2,1,1]
+1class Solution {
+2public:
+3    void sortColors(vector<int>& nums) {
+4        int n=nums.size();
+5        int count0=0;
+6        int count1=0;
+7        int count2=0;
+8        for(int i=0;i<n;i++)
+9        {
+10            if(nums[i]==0)
+11            {
+12                count0++;
+13            }
+14            else if(nums[i]==1)
+15            {
+16                count1++;
+17            }
+18            else
+19            {
+20                count2++;
+21            }
+22        }
+23        int i=0;
+24        
+25            while(count0>0)
+26            {
+27                nums[i]=0;
+28                i++;
+29                count0--;
+30            }
+31            while(count1>0)
+32            {
+33                nums[i]=1;
+34                i++;
+35                count1--;
+36            }
+37            while(count2>0)
+38            {
+39                nums[i]=2;
+40                i++;
+41                count2--;
+42            }
+43        
+44    }
+45};
