f630b60ebd10de2dd9c1bd137c68de9fcd539afa
ananya07 <ambasthaananya@gmail.com>
2026-09-17 23:05:12 +0530

    Add solution for Uncommon Words from Two Sentences

--- LeetCode/Uncommon-Words-from-Two-Sentences/Solution.cpp
+++ LeetCode/Uncommon-Words-from-Two-Sentences/Solution.cpp
@@ -1,9 +1,9 @@
 1class Solution {
 2public:
 3    vector<string> uncommonFromSentences(string s1, string s2) {
-4        unordered_map<string,int>mp;
-5        vector<string> v;
-6        string word="";
+4        unordered_map<string,int> mp;
+5        string word="";
+6        vector<string>v;
 7        for(int i=0;i<=s1.size();i++)
 8        {
 9            if(i==s1.size()||s1[i]==' ')
@@ -12,26 +12,31 @@
 12                word="";
 13            }
 14            else
-15            word+=s1[i];
-16        }
-17        word="";
-18        for(int i=0;i<=s2.size();i++)
-19        {
-20            if(i==s2.size()||s2[i]==' ')
-21            {
-22                mp[word]++;
-23                word="";
-24            }
-25            else
-26            word+=s2[i];
-27        }
-28        for(auto x:mp)
-29        {
-30            if(x.second==1)
-31            {
-32                v.push_back(x.first);
-33            }
-34        }
-35        return v;
-36    }
-37};
+15            {
+16                word+=s1[i]
+17;            }
+18        }
+19        word="";
+20        for(int i=0;i<=s2.size();i++)
+21        {
+22            if(i==s2.size()||s2[i]==' ')
+23            {
+24                mp[word]++;
+25                word="";
+26            }
+27            else
+28            {
+29                word+=s2[i];
+30            }
+31        }
+32        for(auto x:mp)
+33        {
+34            if(x.second==1)
+35            {
+36                v.push_back(x.first);
+37            }
+38
+39        }
+40        return v;
+41    }
+42};
