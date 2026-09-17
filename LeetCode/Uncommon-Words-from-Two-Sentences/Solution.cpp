f02576b6b434733ca9a8809c57ae9f08450c269f
ananya07 <ambasthaananya@gmail.com>
2026-09-17 23:13:23 +0530

    Add solution for Uncommon Words from Two Sentences

--- LeetCode/Uncommon-Words-from-Two-Sentences/Solution.cpp
+++ LeetCode/Uncommon-Words-from-Two-Sentences/Solution.cpp
@@ -13,30 +13,29 @@
 13            }
 14            else
 15            {
-16                word+=s1[i]
-17;            }
-18        }
-19        word="";
-20        for(int i=0;i<=s2.size();i++)
-21        {
-22            if(i==s2.size()||s2[i]==' ')
-23            {
-24                mp[word]++;
-25                word="";
-26            }
-27            else
-28            {
-29                word+=s2[i];
-30            }
-31        }
-32        for(auto x:mp)
-33        {
-34            if(x.second==1)
-35            {
-36                v.push_back(x.first);
-37            }
-38
-39        }
-40        return v;
-41    }
-42};
+16                word+=s1[i];            }
+17        }
+18        word="";
+19        for(int i=0;i<=s2.size();i++)
+20        {
+21            if(i==s2.size()||s2[i]==' ')
+22            {
+23                mp[word]++;
+24                word="";
+25            }
+26            else
+27            {
+28                word+=s2[i];
+29            }
+30        }
+31        for(auto x:mp)
+32        {
+33            if(x.second==1)
+34            {
+35                v.push_back(x.first);
+36            }
+37
+38        }
+39        return v;
+40    }
+41};
