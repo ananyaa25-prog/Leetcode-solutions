d2dacd0dbe6850ff455c491e7ecde7be2958c49c
ananya07 <ambasthaananya@gmail.com>
2026-09-18 21:39:31 +0530

    Add solution for First Unique Character in a String

---
+++ LeetCode/First-Unique-Character-in-a-String/Solution.cpp
@@ -0,0 +1,19 @@
+1class Solution {
+2public:
+3    int firstUniqChar(string s) {
+4      unordered_map<char,int>mp;
+5      for(int i=0;i<s.size();i++)
+6      {
+7        mp[s[i]]++;
+8      }  
+9      for(int i=0;i<s.size();i++)
+10      {
+11        if(mp[s[i]]==1)
+12        {
+13            return i;
+14        }
+15        
+16      }
+17      return -1;
+18    }
+19};
