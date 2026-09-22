9940c80c7a971f76c2cb249e8a1e4207dcdaaf9b
ananya07 <ambasthaananya@gmail.com>
2026-09-22 21:59:28 +0530

    Add solution for Merge Two Sorted Lists

--- LeetCode/Merge-Two-Sorted-Lists/Solution.cpp
+++ LeetCode/Merge-Two-Sorted-Lists/Solution.cpp
@@ -11,30 +11,33 @@
 11class Solution {
 12public:
 13    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-14      ListNode* dummy=new ListNode(0);
-15      ListNode* curr=dummy;
-16      while(list1!=NULL && list2!=NULL)
-17      {
-18        if(list1->val<list2->val)
-19        {
-20            curr->next=list1;
-21            list1=list1->next;
-22        }
-23        else
-24        {
-25            curr->next=list2;
-26            list2=list2->next;
-27        }
-28        curr=curr->next;
-29      }
-30      if(list1!=NULL)
-31      {
-32        curr->next=list1;
-33      }  
-34      if(list2!=NULL)
-35      {
-36        curr->next=list2;
-37      }
-38      return dummy->next;
-39    }
-40};
+14        ListNode* dummy=new ListNode(0);
+15        ListNode* curr=dummy;
+16
+17        while(list1!=NULL && list2!=NULL)
+18        {
+19            if(list1->val<list2->val)
+20            {
+21                curr->next=list1;
+22                list1=list1->next;
+23            }
+24            else 
+25            {
+26                curr->next=list2;
+27                list2=list2->next;
+28            }
+29            curr=curr->next;
+30        }
+31            if(list1!=NULL)
+32            {
+33                curr->next=list1;
+34            }
+35            if(list2!=NULL)
+36            {
+37                curr->next=list2;
+38            }
+39        
+40        return dummy->next;
+41       
+42    }
+43};
