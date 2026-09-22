cf93bc750ae133cb9a983a30d9775e605194c42f
ananya07 <ambasthaananya@gmail.com>
2026-09-22 21:32:24 +0530

    Add solution for Find First and Last Position of Element in Sorted Array

--- LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.cpp
+++ LeetCode/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.cpp
@@ -1,12 +1,12 @@
 1class Solution {
 2public:
 3    vector<int> searchRange(vector<int>& nums, int target) {
-4       int left=0;
-5       int right=nums.size()-1;
-6       int first=-1;
-7       int last=-1;
-8       while(left<=right)
-9       {
+4      int left=0;
+5      int right=nums.size()-1;
+6      int first=-1;
+7      int last=-1;
+8      while(left<=right)
+9      {
 10        int mid=left+(right-left)/2;
 11        if(nums[mid]==target)
 12        {
@@ -14,23 +14,23 @@
 14            right=mid-1;
 15        }
 16        else if(nums[mid]<target)
-17        {
-18            left=mid+1;
-19        }
-20        else
-21        {
-22            right=mid-1;
-23        }
-24       }
-25       left=0;
-26       right=nums.size()-1;
-27       while(left<=right)
-28       {
+17      {
+18        left=mid+1;
+19      }
+20      else
+21      {
+22        right=mid-1;
+23      }
+24      }
+25      left=0;
+26      right=nums.size()-1;
+27      while(left<=right)
+28      {
 29        int mid=left+(right-left)/2;
 30        if(nums[mid]==target)
 31        {
-32            last=mid;
-33            left=mid+1;
+32        last=mid;
+33        left=mid+1;
 34        }
 35        else if(nums[mid]<target)
 36        {
@@ -40,8 +40,7 @@
 40        {
 41            right=mid-1;
 42        }
-43       }
-44     return{first,last};
+43      } 
+44      return {first,last}; 
 45    }
-46   
-47};
+46};
