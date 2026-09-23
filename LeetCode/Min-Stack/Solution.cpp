77a51d5e0ca9a6ab891ebcc975915d874588988b
ananya07 <ambasthaananya@gmail.com>
2026-09-23 22:08:04 +0530

    Add solution for Min Stack

--- LeetCode/Min-Stack/Solution.cpp
+++ LeetCode/Min-Stack/Solution.cpp
@@ -1,5 +1,5 @@
 1class MinStack {
-2    stack<long long>st;
+2    stack<long long> st;
 3    long long minV=LLONG_MAX;
 4public:
 5    MinStack() {
@@ -7,56 +7,58 @@
 7    }
 8    
 9    void push(int value) {
-10       if(st.empty())
-11       {
+10     if(st.empty())
+11     {
 12        minV=value;
 13        st.push(value);
-14       } 
-15       else
-16       {
+14     } 
+15     else
+16     {
 17        if(value>minV)
 18        {
 19            st.push(value);
 20        }
 21        else
 22        {
-23           st.push(2LL*value-minV);
-24           minV=value;
+23            st.push(2LL*value-minV);
+24            minV=value;
 25        }
-26       }
+26     }  
 27    }
 28    
 29    void pop() {
-30       if(st.empty())
-31       {
-32        return;
-33       } 
+30        if(st.empty())
+31        {
+32            return;
+33        }
 34        long long x=st.top();
 35        st.pop();
 36        if(x<minV)
-37        minV=2LL*minV-x;
-38    }
-39    
-40    int top() {
-41        if(st.empty())
-42        {
-43            return -1;
-44        }
-45        long long x=st.top();
-46        if(minV<x) return x;
-47        return minV;
-48    }
-49    
-50    int getMin() {
-51        return minV;
-52    }
-53};
-54
-55/**
-56 * Your MinStack object will be instantiated and called as such:
-57 * MinStack* obj = new MinStack();
-58 * obj->push(value);
-59 * obj->pop();
-60 * int param_3 = obj->top();
-61 * int param_4 = obj->getMin();
-62 */
+37        {
+38            minV=2LL*minV-x;
+39        }
+40    }
+41    
+42    int top() {
+43      if(st.empty())
+44      {
+45        return -1;
+46      }
+47      long long x=st.top();
+48      if(minV<x) return x;
+49      return minV;
+50    }
+51    
+52    int getMin() {
+53       return minV; 
+54    }
+55};
+56
+57/**
+58 * Your MinStack object will be instantiated and called as such:
+59 * MinStack* obj = new MinStack();
+60 * obj->push(value);
+61 * obj->pop();
+62 * int param_3 = obj->top();
+63 * int param_4 = obj->getMin();
+64 */
