34912b054988ef24b229f9f2ae1af109038789d5
ananya07 <ambasthaananya@gmail.com>
2026-09-25 22:47:13 +0530

    Add solution for Single Number

--- LeetCode/Single-Number/Solution.cpp
+++ LeetCode/Single-Number/Solution.cpp
@@ -1,36 +1,21 @@
 1class Solution {
 2public:
 3    int singleNumber(vector<int>& nums) {
-4        /*
-5       unordered_map<int,int> mp;
-6       for(int nc:nums)
+4    sort(nums.begin(),nums.end());
+5       int ans;
+6       for(int i=0;i<nums.size();i++)
 7       {
-8        mp[nc]++;
-9       }
-10       for(auto x:mp)
-11       {
-12        if(x.second==1)
-13        {
-14            return x.first;
-15        }
-16       }
-17       return{};
-18       */
-19       sort(nums.begin(),nums.end());
-20       int ans;
-21       for(int i=0;i<nums.size();i++)
-22       {
-23        int count=1;
-24        while(i+1<nums.size() && nums[i]==nums[i+1])
-25        {
-26            count++;
-27            i++;
-28        }
-29        if(count==1)
-30         {
-31           ans=nums[i];
-32         }
-33       }
-34       return ans;
-35    }
-36};
+8        int count=1;
+9        while(i+1<nums.size() && nums[i]==nums[i+1])
+10        {
+11            count++;
+12            i++;
+13        }
+14        if(count==1)
+15         {
+16           ans=nums[i];
+17         }
+18       }
+19       return ans;
+20    }
+21};
